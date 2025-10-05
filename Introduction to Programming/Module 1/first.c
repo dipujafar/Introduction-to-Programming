f3e138b949edbdbc6650fbbf7fda65efef6a3762
dipujafar <dipujafarmr@gmail.com>
2025-10-05 23:49:52 +0600

    start c

---
+++ Introduction to Programming/Module 1/first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int num1 = 10;
+    float f = 10.545488;
+    char ch = 'a';
+    // printf("%c", ch);
+    // printf("%d", num1);
+    printf("%.2f", f);
+    return 0;
+}
+
+// formate  specifier
+
+// for integer %d
+// for character %c
+// for float %f
---
+++ Introduction to Programming/Module 1/input.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    char a;
+    float f;
+    scanf("%d %c %f", &num, &a, &f);
+    printf("%d %c %f", num, a, f);
+    return 0;
+}
